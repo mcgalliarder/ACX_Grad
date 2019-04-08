8ee9463c973ab1382bae90377fe2f172a31986f6
Eli R. McGalliard <eli.mcgalliard@gmail.com>
2019-04-08 18:55:11 -0400

    Working product
    Made changes to the ret.addr to fix issues with return address for stack pointer

--- ACX_Grad/ACX_Grad/acx.c
+++ ACX_Grad/ACX_Grad/acx.c
@@ -123,18 +123,18 @@ void x_init(void)
 
     // create a PTUnion and save the return address
     volatile PTUnion ret;
-    ret.addr[0] = * (stackP + 8);
+    ret.addr[0] = * (stackP + 10);
     ret.addr[1] = * (stackP + 9);
-    ret.addr[2] = * (stackP + 10);
+    ret.addr[2] = * (stackP + 8);
 
     // change the stack pointer to the bottom of T0
     SP = (int) (mem + T0_STACK_BASE_OFFS);
 
     // push the old return address onto the new stack
 	int stackpointer = SP - (int) mem;
-	mem[stackpointer--] = ret.addr[2];
-	mem[stackpointer--] = ret.addr[1];
 	mem[stackpointer--] = ret.addr[0];
+	mem[stackpointer--] = ret.addr[1];
+	mem[stackpointer--] = ret.addr[2];
 	SP = SP - 10;
 	
 
@@ -180,15 +180,18 @@ void x_new(uint8_t ID, PTHREAD thread, bool enable) {
 	cli();
 	//volatile PTUnion ret = {*thread};
 	volatile PTUnion ret;
+	ret.addr[2] = 0;
+	ret.addr[1] = 0;
+	ret.addr[0] = 0;
 	ret.pthread = thread;
 	
 	int stackpointer = stackControlTable[ID].spBase - (int) mem;
 	
-	mem[stackpointer--] = ret.addr[2];
-	mem[stackpointer--] = ret.addr[1];
 	mem[stackpointer--] = ret.addr[0];
+	mem[stackpointer--] = ret.addr[1];
+	mem[stackpointer--] = ret.addr[2];
 	
-	stackControlTable[ID].sp = stackControlTable[ID].spBase - 21; //18
+	stackControlTable[ID].sp = stackControlTable[ID].spBase - 22; //18
 	
 	char disablebit = enable << ID;
 	disable = (enable) ? disable & ~disablebit : disable | disablebit ;
